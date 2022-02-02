5d8abb074be36d342d60a22e1fbf635889149105
Tobias Fischer <info@tobiasfischer.info>
2022-02-03 07:29:56 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,6 @@
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 if [ "${target_platform}" == "linux-aarch64" ]; then
     EXTRA_ARGS="--host=aarch64-unknown-linux-gnu --build=aarch64-unknown-linux-gnu"
 fi
